5893eea42f257011aad32f959073c5b3a5ff80ae
fudzior <73337812+fudzior@users.noreply.github.com>
2021-07-27 23:00:48 +0200

    Add files via upload
    class Word divided into Word and Game, still to improve

--- hangman.cpp
+++ hangman.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 
 
-class Word
+/*class Word
 {
     private:
         std::string hidden_string;
@@ -33,19 +33,17 @@ class Word
             std:: cout << std::endl;
         }
 
-        int check_letters_number()
+        void check_letters_number()
         {
             letters_number = hidden_string.size();
             letters_to_find = letters_number;
-            return letters_number, letters_to_find;
         }
 
-        auto prepare_found_word_vector()
+        void prepare_found_word_vector()
         {
             int new_size = hidden_string.size();
             found_word.resize(new_size);
             std::fill(begin(found_word), end(found_word), '_');
-            return found_word;
         }
 
         bool game_over()
@@ -54,20 +52,20 @@ class Word
             {
                 std::cout << "Game over, you lost." << std::endl;
                 std::cout << "Hidden word was: " << full_hidden_string << std::endl;
-                return 1;
+                return true;
             }
             else if (letters_to_find == 0)
             {
                 std::cout << "Game over, you won." << std::endl;
-                return 1;
+                return true;
             }
             else
             {
-                return 0;
+                return false;
             }
         }
 
-        auto check_letter()
+        void check_letter()
         {
             auto letter_iterator = std::find(begin(hidden_string), end(hidden_string), letter);
 
@@ -78,10 +76,10 @@ class Word
             }
             else
             {
+                char dash = '-';
                 while (letter_iterator != std::end(hidden_string))
                 {
                     int letter_position = letter_iterator - hidden_string.begin();
-                    char dash = '-';
                     found_word[letter_position] = letter;
                     hidden_string[letter_position] = dash;
                     --letters_to_find;
@@ -121,7 +119,7 @@ class Word
                 check_letter();
             }
         }
-};
+};*/
 
 
 struct wordGenerator
@@ -130,14 +128,14 @@ struct wordGenerator
     std::string word;
     unsigned char letter_in_text;
     int letter_position;
+    const int min_word_size = 6;
 
-    public:
     wordGenerator(const std::string& text_):
         text(text_),
         letter_position(0)
         {};
 
-    auto generate()
+    std::string generate()
     {
         word.clear();
         word.resize(0);
@@ -153,7 +151,7 @@ struct wordGenerator
             }
             else
             {
-                if (word.size()<6)
+                if (word.size()<min_word_size)
                 {
                     word.erase();
                     word.resize(0);
@@ -163,35 +161,36 @@ struct wordGenerator
                     letter_position = i;
                     //std::cout << "Generated word: " << word << std::endl;
                     //std::cout << "Letter position: " << letter_position << std::endl;
-                    return word, letter_position;
+                    return word; //;
                 }
             }
         }
+        return word; // not necessary, to avoid complier warning "control reaches end of non-void function -wreturn-type"
     }
 };
 
-bool play_again()
+/*bool play_again()
 {
-    bool ask_again = 1;
+    bool ask_again = true;
     unsigned char c;
 
-    while (1)
+    while (true)
     {
         std::cout << "Do you want to play again? y/n" << std::endl;
         std::cin >> c;
 
         if (c == 'y' || c == 'Y')
         {
-            return 1;
+            return true;
         }
         else if (c == 'n' || c == 'N')
         {
-            return 0;
+            return false;
         }
         else
         {}
     }
-}
+} */
 
 std::string text_generator()
 {
@@ -216,22 +215,179 @@ std::string text_generator()
     return some_text;
 }
 
+
+
+
+//////////////////////////////
+
+
+struct Game
+{
+    int life_number = 5;
+    int letters_to_find;
+    std::string full_hidden_string;
+
+    virtual ~Game(){};
+    virtual void word_setup() = 0;
+    virtual void check_letter() = 0;
+
+   void play_game()
+    {
+        word_setup();
+        while (!game_over())
+        {
+            check_letter();
+        }
+    }
+
+    bool game_over()
+    {
+        if (life_number == 0)
+        {
+            std::cout << "Game over, you lost." << std::endl;
+            std::cout << "Hidden word was: " << full_hidden_string << std::endl;
+            return true;
+        }
+        else if (letters_to_find == 0)
+        {
+            std::cout << "Game over, you won." << std::endl;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool play_again()
+    {
+        bool ask_again = true;
+        unsigned char c;
+
+        while (true)
+        {
+            std::cout << "Do you want to play again? y/n" << std::endl;
+            std::cin >> c;
+
+            if (c == 'y' || c == 'Y')
+            {
+                return true;
+            }
+            else if (c == 'n' || c == 'N')
+            {
+                return false;
+            }
+            else
+            {}
+        }
+    }
+};
+
+
+class Word: public Game
+{
+    public:
+        std::string hidden_string;
+        //std::string full_hidden_string;
+        int letters_number;
+        //int letters_to_find;
+        //int life_number;
+
+    public:
+        unsigned char letter;
+        std::vector<char> found_word{};
+
+        Word(const std::string& word):
+            hidden_string(word)
+            //life_number(5)
+            {};
+
+        void print_found_word()
+        {
+            std:: cout << "Found word is: " << std::endl;
+            for (auto c : found_word) std::cout << c << ' ';
+            std:: cout << std::endl;
+        }
+
+        void check_letters_number()
+        {
+            letters_number = hidden_string.size();
+            letters_to_find = letters_number;
+        }
+
+        void prepare_found_word_vector()
+        {
+            int new_size = hidden_string.size();
+            found_word.resize(new_size);
+            std::fill(begin(found_word), end(found_word), '_');
+        }
+
+        unsigned char get_letter()
+        {
+            unsigned char given_letter_= '-';
+            while (!(std::isalpha(given_letter_)))
+            {
+                std::cout << "Give me a letter" << std::endl;
+                std::cin >> given_letter_;
+                }
+            letter = std::tolower(given_letter_);
+            return letter;
+        }
+
+
+        void check_letter() override
+        {
+            get_letter();
+
+            auto letter_iterator = std::find(begin(hidden_string), end(hidden_string), letter);
+
+            if (letter_iterator == std::end(hidden_string))
+            {
+                std::cout << "Letter " << letter << " not found" <<std::endl;
+                --life_number;
+            }
+            else
+            {
+                char dash = '-';
+                while (letter_iterator != std::end(hidden_string))
+                {
+                    int letter_position = letter_iterator - hidden_string.begin();
+                    found_word[letter_position] = letter;
+                    hidden_string[letter_position] = dash;
+                    --letters_to_find;
+                    letter_iterator = std::find(begin(hidden_string), end(hidden_string), letter);
+                }
+            }
+           print_found_word();
+        }
+
+        void word_setup() override
+        {
+            check_letters_number();
+            prepare_found_word_vector();
+            print_found_word();
+            full_hidden_string = hidden_string;
+        }
+};
+
+
+//////////////////////////
+
 int main()
 {
     std::string text = text_generator();
     wordGenerator word_generator(text);
 
     std::string word;
-    bool play_again_ = 1;
+    bool play_again_ = true;
 
     while (play_again_)
         {
-        word_generator.generate();
-        word = word_generator.word;
+        word = word_generator.generate();
 
         Word temp_word(word);
         temp_word.play_game();
-        play_again_ = play_again();
+        play_again_ = temp_word.play_again();
         }
 
     return 0;
